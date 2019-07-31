7aa2e32fe27e06841a6aae1010e32636b282dec2
Guy Giat <guy.giat@cyberark.com>
2019-07-31 17:46:05 +0300

    Add host flag

--- lib/conjur/command/enable.rb
+++ lib/conjur/command/enable.rb
@@ -32,7 +32,7 @@ class Conjur::Command::Enable < Conjur::Command
         filename = 'integrations/jenkins.yml'
         require 'open-uri'
         file = open(filename).read
-        policy = file.gsub("<HOST>", :host.to_sym)
+        policy = file.gsub("<HOST>", :host.to_s)
         
         method = Conjur::API::POLICY_METHOD_POST
         result = api.load_policy policy_id, policy, method: method
